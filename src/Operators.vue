f91f6f88d99ea524e577115bb5d7e15734181d9c
AlexeyShaykov <kayfolom@inbox.ru>
2018-02-21 12:08:27 +0300

    Update Operators.vue

--- src/Operators.vue
+++ src/Operators.vue
@@ -32,7 +32,13 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
+computed: {		
+ -    ...mapGetters({		
+ -      operators: 'getOperators'		
+ -    })		
+ -  },
 };
 </script>
 
